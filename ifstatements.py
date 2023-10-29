5aad9992665b8c3ac4aec9880bdd8209d80d3492
bulletzenith <inkworkero@gmail.com>
2023-10-29 07:59:26 +0300

    add ifstatements.py

---
+++ ifstatements.py
@@ -0,0 +1,39 @@
+# age = input('What is your age?\n> ')
+# if age >= 18:
+#     print("You are old enough to vote")
+# else:
+#     print("You are not old enough to vote.")
+
+# name = input('What is your name?\n> ')
+# if name == "Nina":
+#     print("Hello Nina!")
+# else:
+#     print("Hello stranger")
+
+# singer = input('Welcome user\nWho is your favorite singer?\n> ')
+# if singer == "Beyonce":
+#     print("Good singer (false)")
+# elif singer == "Ed":
+#     print("Meh")
+# else:
+#     print("Git gud")
+
+# grade = int(input('What percentage did you get in your exam?\n> '))
+# if grade >= 90:
+#     print("Thats a Grade 9 / A**... good stuff")
+# elif grade >= 80:
+#     print("Thats a Grade 8 / A*... man you can get better next time")
+# elif grade >= 70:
+#     print("Thats a Grade 7 / A... thats pretty average")
+# else:
+#     print("Pass")
+
+lockdown = input("Is there a lockdown?\n> ")
+if lockdown == "no":
+    print("Have a nice day")
+else:
+    home = input("Are you at home?")
+    if home == "yes":
+        print("Well done, stay safe")
+    else:
+        print("Go home")
