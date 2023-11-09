effb2375b5f1331a22a6f5379f3642c82c7707d9
bulletzenith <inkworkero@gmail.com>
2023-11-09 10:26:44 +0300

    finish ifstatementspart3.py

--- ifstatementspart3.py
+++ ifstatementspart3.py
@@ -1,5 +1,28 @@
-choice = input("Would you like to convert to Celsius or Fahrenheit?\n> ")
-if choice == "Celsius":
-    print((int(input("Enter a temperature in Fahrenheit\n> ")) * 9 / 5) + 32)
-if choice == "Fahrenheit":
-    print(((int(input("Enter a temperature in Celsius\n> "))) - 32) * 5 / 9)
+# choice = input("Would you like to convert to Celsius or Fahrenheit?\n> ")
+# if choice == "Celsius":
+#     print((int(input("Enter a temperature in Fahrenheit\n> ")) * 9 / 5) + 32)
+# if choice == "Fahrenheit":
+#     print(((int(input("Enter a temperature in Celsius\n> "))) - 32) * 5 / 9)
+# 
+# 
+# 
+# gamename = input("what is your favorite game?\n> ")
+# if gamename == "Guild Wars":
+#     print("Old but good!")
+# else:
+#     gameyear = int(input("What year was it released?\n> "))
+#     if year > 2010:
+#         print("Decent!")
+#     else:
+#         print("Good old games...")
+
+miles = int(input("How many miles do you have to drive?\n> "))
+hasMembershipCard = input("Do you have a membership card?\n> ")
+price = 0
+if hasMembershipCard == "yes":
+    price = 2 + ((miles - 1) * 0.25)
+else:
+    price = 2 + ((miles - 1) * 0.25) + 5
+dieselCost = miles * 0.5
+totalCost = price + dieselCost
+print(f"Your total cost is ${totalCost}")
